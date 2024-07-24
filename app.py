607669b6958ecebff1cb7d83ddf62bc0258bf31a
juliaozm <ozmiteljul@gmail.com>
2024-07-25 00:43:13 +0100

    Implemented interface to enter dates for filtering transactions in the period. Used Pandas to provide a readable format for selected datasets.

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
-
 from db import Database
+from data.users import users
+from data.accounts import accounts
+from data.statements import statements
+from data.categories import categories
+from data.transactions import transactions
+from transactions.transaction_service import TransactionService
 
-db= Database('bankdb.sqlite')
+db = Database('bankdb.sqlite')
 db.create_tables()
+db.insert_users(users)
+db.insert_accounts(accounts)
+db.insert_statements(statements)
+db.insert_categories(categories)
+db.insert_transactions(transactions)
+
+ts = TransactionService(db)
+start_date = input("Enter start date, example 2023-01-10: ")
+end_date = input("Enter end date, example 2023-02-01: ")
+transaction_in_period = ts.get_data_period(start_date, end_date)
+framed_data = ts.convert_to_pd(transaction_in_period)
+print(framed_data)
--- transactions/transaction_service.py
+++ transactions/transaction_service.py
@@ -1,5 +1,53 @@
+from sqlite3 import Error
+import pandas as pd
+
 class TransactionService:
+
     def __init__(self, db):
         self.db = db
 
+    def get_data_period(self, start_day, end_day):
+        insert_sql = """
+            SELECT 
+                t.transaction_id,
+                t.transaction_date,
+                t.description,
+                t.money_out,
+                t.money_in,
+                t.balance,
+                a.account_number,
+                c.name
+            FROM 
+                transactions t
+            JOIN 
+                accounts a ON t.account_id = a.account_id
+            JOIN 
+                categories c ON t.category_id = c.category_id
+            WHERE t.transaction_date 
+            BETWEEN ? AND ?;
+        """
+        try:
+            cursor = self.db.connection.cursor()
+            cursor.execute(insert_sql, [start_day, end_day])
+            result = cursor.fetchall()
+            print(result)
+            return result
+        except Error as e:
+            print(e)
+        finally:
+            cursor.close()
+
+    def convert_to_pd(self, dataset):
+        df = pd.DataFrame(dataset)
+        df.columns = [
+            "ID",
+            "Date",
+            "Description",
+            "Money_out",
+            "Money_in",
+            "Balance",
+            "Account number",
+            "Category name"
+        ]
+        return df
   
